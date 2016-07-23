e113329d28b6ad62a59e79c6a24f0705006cbd2f
VWPole <karolschnur@yahoo.de>
2016-07-23 13:32:05 +0200

    fixed untracked files

--- PokemonGo.RocketAPI.Console/bin/Debug/satrt.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-:start
-ping -n 11 127.0.0.1 > nul
-start PokemonGo.RocketAPI.Console.exe
-ping -n 1801 127.0.0.1 > nul
-TASKKILL /F /IM PokemonGo.RocketAPI.Console.exe
-goto start
-PAUSE
